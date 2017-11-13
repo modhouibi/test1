a2b9eb0613846878e5412174d0781676d6b1cad6
modhouibi <m.dhouibi.dev@gmail.com>
2017-11-13 01:49:15 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express=require('express');
+const app=express();
+const hbs=require('hbs');
+const fs=require('fs')
+
+//other middlewares
+app.use((req,res,next)=>{
+
+var now=new Date().toString();
+fs.appendFile('serverLog.txt',`${req.method} ...${req.path}..${now}`,(err)=>{
+    if(err)
+    throw err;
+    next();
+});
+
+
+})
+
+// app.use((req,res,next)=>{
+//     res.render('mainenance');
+// })
+app.use(express.static(__dirname+'/public'));
+
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('currentYear',()=>{
+return new Date().getFullYear();
+})
+
+app.get('/help.html',(req,res)=>{
+
+   res.send("ww"); 
+})
+
+app.get('/about',(req,res)=>{
+    
+      res.render('about',{hh:123}) 
+    })
+
+app.listen(3000)
